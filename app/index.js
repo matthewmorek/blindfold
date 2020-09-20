18b180a60fa9efa4ba212b52a5ddbade8d00aec2
Matthew Morek <matthew@madbit.co>
2020-09-20 21:35:58 +0100

    🔥 Remove redundant cookie-parser

--- app/index.js
+++ app/index.js
@@ -5,9 +5,7 @@ const promiseLimit = require("promise-limit");
 const express = require("express");
 const sanitizer = require("express-sanitizer");
 const session = require("express-session");
-const cookieParser = require("cookie-parser");
-const redis = require("redis");
-const redisClient = redis.createClient(process.env.REDIS_URL);
+const redisClient = require("redis").createClient(process.env.REDIS_URL);
 const redisStore = require("connect-redis")(session);
 const config = require("./config");
 const path = require("path");
@@ -97,8 +95,6 @@ export default (app) => {
       store: new redisStore({ client: redisClient }),
     })
   );
-
-  app.use(cookieParser(config.salt));
 
   app.use(passport.initialize());
   app.use(passport.session());
