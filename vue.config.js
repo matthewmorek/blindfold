13f4822acdc092ce4cc43d7e13368689c2aa689f
Matthew Morek <matthew@madbit.co>
2020-10-13 13:47:06 +0100

    🚧 Try something with ServiceWorkers

--- vue.config.js
+++ vue.config.js
@@ -5,6 +5,7 @@ module.exports = {
     const svgRule = config.module.rule("svg");
     svgRule.uses.clear();
     svgRule.use("vue-svg-loader").loader("vue-svg-loader");
+    config.plugins.delete("split-manifest").delete("inline-manifest");
   },
 
   productionSourceMap: false,
